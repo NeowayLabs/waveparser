65ff9256cff7eb3c7c7d195c8c6b87a86d6776ac
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-11-27 16:06:52 -0200

    Add first draft of design and implemetation for float

--- waveparser.go
+++ waveparser.go
@@ -1,6 +1,7 @@
 package waveparser
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -33,7 +34,69 @@ type (
 		FirstSamplePos uint32 // position of start of sample data
 		DataBlockSize  uint32 // size of sample block (PCM data)
 	}
+
+	Wav struct {
+		Header *WavHeader
+		Data   []byte
+	}
 )
+
+func Load(audiofile string) (*Wav, error) {
+	f, err := os.Open(audiofile)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	hdr, err := parseHeader(f)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Wav{
+		Header: hdr,
+		Data:   data,
+	}, nil
+}
+
+func (w *Wav) Int16LESamples() ([]int16, error) {
+	// TODO: validate using header
+	audio := []int16{}
+	for i := 0; i < len(w.Data)-1; i += 2 {
+		sample := int16(binary.LittleEndian.Uint16(w.Data[i : i+2]))
+		audio = append(audio, sample)
+	}
+	return audio, nil
+}
+
+func (w *Wav) Float32LESamples() ([]float32, error) {
+	// TODO: validate using header
+	const typesize = 4
+
+	audio := []float32{}
+	reader := bytes.NewBuffer(w.Data)
+	var err error
+
+	for err == nil {
+		var sample float32
+		err = binary.Read(reader, binary.LittleEndian, &sample)
+		if err == nil {
+			audio = append(audio, sample)
+		}
+	}
+
+	if err != io.EOF {
+		return nil, fmt.Errorf("error[%s] loading audio as float32 samples", err)
+	}
+
+	return audio, nil
+}
 
 func (hdr *WavHeader) String() string {
 	strs := []string{
@@ -133,32 +196,4 @@ func parseHeader(r io.ReadSeeker) (*WavHeader, error) {
 		FirstSamplePos: uint32(pos),
 		DataBlockSize:  uint32(chunkSize),
 	}, nil
-}
-
-func LoadAudio(audiofile string) (*WavHeader, []int16, error) {
-	audio := []int16{}
-	f, err := os.Open(audiofile)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	defer f.Close()
-
-	hdr, err := parseHeader(f)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// header already skipped
-	data, err := ioutil.ReadAll(f)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	for i := 0; i < len(data)-1; i += 2 {
-		sample := binary.LittleEndian.Uint16(data[i : i+2])
-		audio = append(audio, int16(sample))
-	}
-
-	return hdr, audio, nil
 }
